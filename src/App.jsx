533643e3a2e2f372ce28dadd044bcc7bee371033
PushkarShinde <pushkarshinde249@gmail.com>
2026-01-21 23:38:01 +0530

    Added routes, orders and checkout pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
+import { Route, Routes } from 'react-router'
 import './App.css'
 import HomePage from './pages/HomePage'
+import CheckoutPage from './pages/CheckoutPage'
+import OrdersPage from './pages/OrdersPage'
 
 function App() {
 
   return (
     <>
-      <HomePage/>
+      <Routes>
+        <Route path="/" element={<HomePage/>}/>
+        <Route path="/checkout" element={<CheckoutPage/>}/>
+        <Route path="/orders" element={<OrdersPage/>}/>
+      </Routes>
     </>
   )
 }
